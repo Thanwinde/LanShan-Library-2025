735f767cc95a23f14b166600f686b9b7a5596362
Thanwinde <Thanwinde@outlook.com>
2025-02-10 17:20:22 +0800

    2025.2.10第十一次更新，添加了OAuth2+JWT版，session版也加入了oauth2
    2025.2.10第十一次更新，添加了OAuth2+JWT版，支持通过GitHub登录，在session版也加入GitHub登录，后续会尝试支持从QQ,短信验证码登录

---
+++ LanShanLibrary/src/main/java/com/LanShan/Library/service/handler/LogoutHandler.java
@@ -0,0 +1,24 @@
+package com.LanShan.Library.service.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+//登出的handler
+@Component
+public class LogoutHandler implements LogoutSuccessHandler {
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request,
+    HttpServletResponse response, Authentication authentication) throws IOException{
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"message\": \"成功登出!\"}");
+        response.getWriter().flush();
+    }
+
+}
---
+++ LanShanLibrary/src/main/java/com/LanShan/Library/service/handler/SuccessLoginHandler.java
@@ -0,0 +1,29 @@
+package com.LanShan.Library.service.handler;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.LanShan.Library.pojo.MyUserDetails;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+//处理登陆成功的返回信息
+@Component
+public class SuccessLoginHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+    Authentication authentication) throws IOException, ServletException {
+        MyUserDetails userDetails = (MyUserDetails) authentication.getPrincipal();
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        JSONObject json = new JSONObject();
+        json.put("message", "登录成功!");
+        json.put("ID", userDetails.getId());
+        json.put("用户名:", userDetails.getUsername());
+        json.put("权限:", userDetails.getAuthorities());
+        response.getWriter().write(json.toJSONString());
+    }
+}
